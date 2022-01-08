fb5279fa2d1eb0fc54082ca981593ff5f00245ff
jo-vana <jovananukic85@gmail.com>
2022-01-08 21:46:42 +0100

    Products section and footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 //All your JS code goes here
+// Back to top btn
+var btn = $('#button');
+
+jQuery(window).on('scroll' ,function() {
+  if (jQuery(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  jQuery('html, body').animate({scrollTop:0}, '300');
+});
+  
