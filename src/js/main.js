4d4f494e92acdf446ccd280d9c88b4ddce945f7c
jo-vana <jovananukic85@gmail.com>
2022-01-10 21:23:18 +0100

    Cart PopUp

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,16 @@ btn.on('click', function(e) {
   e.preventDefault();
   jQuery('html, body').animate({scrollTop:0}, '300');
 });
-  
+// PopUp Cart
+jQuery('#cart').click(function(e) {
+  e.stopPropagation();
+  jQuery(".shopping-cart").toggleClass("active");
+});
+// Close on outher click
+jQuery(document).click(function() {
+  var $item = jQuery(".shopping-cart");
+  if ($item.hasClass("active")) {
+    $item.removeClass("active");
+  }
+});
+
